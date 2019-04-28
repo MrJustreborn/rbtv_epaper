b523961882127eeb5d06d267e84e9bd62343cd9a
René Thomas Lottes <rene.lottes@gmx.de>
2019-04-28 09:04:00 +0200

    Uses relative paths

--- python3/rbtv_config.py
+++ python3/rbtv_config.py
@@ -1,11 +1,17 @@
 
 from PIL import Image,ImageDraw,ImageFont
+import os
 
-fontSmal = ImageFont.truetype('../font/wqy-microhei.ttc', 24)
-fontBig = ImageFont.truetype('../font/wqy-microhei.ttc', 95)
+font = os.path.join(os.path.dirname(__file__), '..', 'font')
+img = os.path.join(os.path.dirname(__file__), '..', 'img')
 
-live = Image.open('../img/live.bmp')
-neu = Image.open('../img/neu.bmp')
+fontSmal = ImageFont.truetype(font + '/wqy-microhei.ttc', 24)
+fontBig = ImageFont.truetype(font + '/wqy-microhei.ttc', 95)
+
+fontAwesome = ImageFont.truetype(font + '/Font Awesome 5 Free-Solid-900.otf', 25)
+
+live = Image.open(img + '/live.bmp')
+neu = Image.open(img + '/neu.bmp')
 
 screen_width = 600
 screen_height = 448
